e600a00fcb296a16bf12aa876bed1ace33de04cd
sahilchauhan11 <sahilsingh982167@gmail.com>
2025-05-24 22:21:40 +0530

    cors change

--- backend/index.js
+++ backend/index.js
@@ -17,18 +17,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = [process.env.URL];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+    origin: (origin, callback) => {
+      // Allow localhost and your frontend deployment URLs
+      const allowedOrigins = [
+        process.env.URL,
+        'http://localhost:3000',
+        'https://your-frontend.vercel.app'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
     credentials: true,
-};
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
   
 app.use(cors(corsOptions));
 
